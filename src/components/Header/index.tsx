e1337fdc4cd13ea06890be3de40d2de017ab70ee
Junior Albuquerque <junioralbuquerque81@gmail.com>
2021-04-26 03:28:39 -0400

    fix: not load data after back home with click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,39 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
+import Router from "next/router";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
+  const [redirect, setRedirect] = useState(false);
+
   const currentDate = format(new Date(), "EEEEEE, d MMMM", {
     locale: ptBR,
   });
 
+  const handleRedirect = () => {
+    if (Router.pathname !== "/") {
+      setRedirect(true);
+    } else {
+      setRedirect(false);
+    }
+  };
+
+  useEffect(() => {
+    setRedirect(Router.pathname !== "/");
+  });
+
   return (
     <header className={styles.headerContainer}>
-      <Link href="/">
-        <img src="/logo.svg" alt="Poadcastr" />
-      </Link>
+      {redirect ? (
+        <Link href="/">
+          <img src="/logo.svg" alt="Poadcastr" onClick={handleRedirect} />
+        </Link>
+      ) : (
+        <img src="/logo.svg" alt="Poadcastr" onClick={handleRedirect} />
+      )}
 
       <p>O melhor para você ouvir sempre</p>
 
