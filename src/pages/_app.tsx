91752d81a23e6d4948df5bcd1f827dbdebac849f
Junior Albuquerque <junioralbuquerque81@gmail.com>
2021-04-25 20:13:22 -0400

    chore: add loading in load pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,73 @@
 import "../styles/global.scss";
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
+import Lottie from "react-lottie";
 
 import styles from "../styles/app.module.scss";
+import React from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+import animationData from "../../public/animations/poad-listen.json";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(false);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  React.useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on("routeChangeStart", () => {
+      NProgress.start(), start();
+    });
+    Router.events.on("routeChangeComplete", () => {
+      NProgress.done(), end();
+    });
+    Router.events.on("routeChangeError", () => {
+      NProgress.done(), end();
+    });
+    return () => {
+      Router.events.off("routeChangeStart", () => {
+        NProgress.start(), start();
+      });
+      Router.events.off("routeChangeComplete", () => {
+        NProgress.done(), end();
+      });
+      Router.events.off("routeChangeError", () => {
+        NProgress.done(), end();
+      });
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <main>
         <Header />
-        <Component {...pageProps} />
+        {loading ? (
+          <div className={styles.animation}>
+            <Lottie
+              options={defaultOptions}
+              height={380}
+              width={380}
+              isClickToPauseDisabled={true}
+            />
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </main>
 
       <Player />
